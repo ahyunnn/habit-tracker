7a2235ca332e9ddeca1e271b68ed5807bb219613
ahyunnn <ahyun3089@gmail.com>
2025-12-11 22:57:02 +0900

    [feat] * change tailwind.config.js setting * create mandalart ui

--- my-project/src/app/template/mandalart/page.tsx
+++ my-project/src/app/template/mandalart/page.tsx
@@ -1,5 +1,16 @@
 const MandalartPage = () => {
-  return <div>Mandalart</div>;
+  return (
+    <div>
+      <div>Mandalart</div>
+      <div className="grid grid-cols-3 gap-2 w-full max-w-md">
+        {[...Array(9)].map((_, i) => (
+          <div key={i} className="border p-4 rounded-lg bg-white text-center">
+            Cell {i + 1}
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default MandalartPage;
