6346c40ff8a0d71270822c5c604c08513e49e30c
ahyunnn <ahyun3089@gmail.com>
2026-01-12 21:34:54 +0900

    feat: create signup page ui

---
+++ frontend/src/app/(auth)/login/page.tsx
@@ -0,0 +1,11 @@
+import { Box, Typography } from "@mui/material";
+
+const LoginPage = () => {
+  return (
+    <Box>
+      <Typography>로그인</Typography>
+    </Box>
+  );
+};
+
+export default LoginPage;
