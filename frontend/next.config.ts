1829f59ce429014d50e676a058e789d141283f6e
ahyun <ahyun3089@gmail.com>
2026-02-22 21:10:15 +0900

    fix: * next.config.ts : basePath, assetPrefix 추가 * package.json : Scripts 수정

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -3,6 +3,9 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   reactCompiler: true,
+  output: "export", // static export
+  basePath: "/habit-tracker",
+  assetPrefix: "/habit-tracker/",
 };
 
 export default nextConfig;
